d9828137d4a3abfbf577ef1f2b0c19a1a3424bb5
Zaurbek Stark <121587026+zaurbek-stark@users.noreply.github.com>
2024-03-17 16:29:29 -0400

    add codebender link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
         <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-[2rem]">
           Super<span className="highlight-text">Viral</span>
         </h1>
+        <p className="author">Built by Zaurbek Stark (<a href="https://lastcodebender.com/bootcamp" target="_blank">The Codebender</a>)</p>
     </div>
   )
 };
